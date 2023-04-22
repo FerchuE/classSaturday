ef0de762dc0e4b288cc6b0ad97bb2acea62d500c
Fernando Estrada <fernandoestrada261@gmail.com>
2023-04-22 11:40:35 -0300

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -81,6 +81,7 @@ module.exports =  {
                 }
 
                 const token = await generatorToken({id:usuario._id, email:usuario.email})
+                console.log ('usuario logeado ' + usuario._id)
                 res.json({msg: "usuario logeado", token})
             } else {
                 res.json(err)
